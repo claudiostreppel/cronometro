66d8a36032730e4e8499aec5b6b0cea1bb8ecc30
Claudio R Streppel <claudio.ricardo.streppel@gmail.com>
2023-02-15 16:27:56 -0300

    projeto finalizado mas achei um bug. Ao pausar o cronometro, fazer o reset, e clicar no iniciar o cronometro não inicia

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,9 @@ let miliseconds = 0
 let isPaused = false
 
 startBtn.addEventListener("click", startTimer)
+pauseBtn.addEventListener("click", pauseTimer)
+resumeBtn.addEventListener("click", resumeTimer)
+resetBtn.addEventListener("click", resetTimer)
 
 function startTimer(){
     interval = setInterval(()=> {
@@ -29,9 +32,51 @@ function startTimer(){
         seconds = 0
     }
 
-    minutesEl.textContent = minutes
-    secondsEl.textContent = seconds
-    milisecondsEl.textContent = miliseconds
+    minutesEl.textContent = formatTime(minutes) 
+    secondsEl.textContent = formatTime(seconds) 
+    milisecondsEl.textContent = formatMiliseconds(miliseconds) 
     }
     },10)
+
+        startBtn.style.display = "none"
+        pauseBtn.style.display = "block"
+
+}
+
+function pauseTimer(){
+    isPaused = true
+    pauseBtn.style.display = "none"
+    resumeBtn.style.display = "block"
+}
+
+function resumeTimer(){
+    isPaused = false
+    pauseBtn.style.display = "block"
+    resumeBtn.style.display = "none"
+    
+}
+
+function resetTimer(){
+    clearInterval(interval)
+    minutes = 0
+    seconds = 0
+    miliseconds = 0
+
+    minutesEl.textContent = "00"
+    secondsEl.textContent = "00"
+    milisecondsEl.textContent = "000"
+
+    startBtn.style.display = "block"
+    pauseBtn.style.display = "none"
+    resumeBtn.style.display= "none"
+}
+
+
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
+
+function formatMiliseconds(time) {
+    return time < 100 ? `${time}`.padStart(3, "0") : time
 }
